15651e3938137a4e8269fd8e2aae8c24c645fb96
Gianluigi Fortini <20071210@mydbs.ie>
2026-01-20 19:50:47 +0000

    commit

--- Lecture 3 OOP/Program.cs
+++ Lecture 3 OOP/Program.cs
@@ -2,43 +2,61 @@
 using System.ComponentModel.DataAnnotations;
 using System.Runtime.InteropServices.Swift;
 using System.Security.Cryptography.X509Certificates;
-public class Shape
+public abstract class Shape
 {
     
 }
 public class Circle : Shape
 {
-    public static double radius;
-    public static double Perimeter()
+    public int radius;
+    public double Perimeter()
     {
         return 6.28*radius;
     }
-    public static double Area()
+    public double Area()
     {
-        return 3.14*radius^2;
+       return 3.14*Math.Pow(radius,2);
     }
 }
 public class Rectangle : Shape
 {
-    public static int length;
-    public static int width;
-    public static int Perimeter()
+    public int length;
+    public int width;
+    public Rectangle(int l, int w)
+    {
+        length = l;
+        width = w;
+    }
+    public int Perimeter()
     {
         return length*2+width*2;
     }
-    public static int Area()
+    public int Area()
     {
         return length*width;
     }
 }
 public class Square : Rectangle
 {
-    public static int Perimeter()
+    public Square(int l) : base(l,l)
     {
-        return length*4;
+        
     }
-    public static int Area()
+}
+public class Program
+{
+    public static void Main(string[] args)
     {
-        return length^2;
+        Shape[] shapes = new Shape[5];
+        shapes[0] = new Rectangle(5,4);
+        shapes[1] = new Square(3);
+        shapes[2] = new Rectangle(6,3);
+        shapes[3] = new Square(5);
+        shapes[4] = new Square(10);
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(shapes[i].Area());
+        }
+        Console.WriteLine(shapes);
     }
 }
